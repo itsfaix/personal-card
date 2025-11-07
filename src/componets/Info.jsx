ac36f43c731789cc6457b129bf91c570b6f8d554
Mortenlost <musicaarhe@gmail.com>
2025-11-07 10:12:16 -0600

    Updated name

--- src/componets/Info.jsx
+++ src/componets/Info.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ian from "/src/assets/ian.png";
+import Faith from "../assets/Faith.png"
 import { FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 export default function Info() {
     return(
         <header>
-            <img className="info-img" src={ian} alt="Ian with glasses looking smiling to the camera with flowers at his side" />
-            <h1 className="name">Ian Aucoin</h1>
+            <img className="info-img" src={Faith} alt="Faith with glasses looking smiling to the camera with flowers at his side" />
+            <h1 className="name">Faith Aucoin</h1>
             <h3 className="role">Frontend Developer</h3>
-            <h4 className="info-website">Ianaucoin.website</h4>
+            <h4 className="info-website">Faithaucoin.website</h4>
             <div className="info-bnt-container">
                 
                 <a className="info-email bnt" target="_blank" href="#">
@@ -17,7 +17,7 @@ export default function Info() {
                 </a>
                 
                 
-                <a className="info-linkedin bnt" target="_blank" href="https://www.linkedin.com/in/ian-aucoin-69515a251/" >
+                <a className="info-linkedin bnt" target="_blank" href="https://www.linkedin.com/in/faith-acuoin/" >
                     <FaLinkedin className="info-icon" />
                     LinkedIn
                 </a>
